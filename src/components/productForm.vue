16f24e03b0492608493eadbfc3799775f61fb96a
Evgenii <voro-98@list.ru>
2022-09-05 20:36:11 +0300

    add adaptive

--- src/components/productForm.vue
+++ src/components/productForm.vue
@@ -44,7 +44,7 @@
         </div>
       </div>
       <button
-        v-on:click="addProduct()"
+        v-on:click="setProduct()"
         v-bind:class="{ active: validForm }"
         class="form__button"
         type="button"
@@ -73,8 +73,8 @@ export default {
     };
   },
 
-  methods:{
-    addProduct() {
+  methods: {
+    setProduct() {
       if (!this.validForm) {
         if (!this.inputName) {
           this.hasName = true;
@@ -104,7 +104,7 @@ export default {
 
       upgradeLocalStorage(this.products);
 
-      this.$emit('add-product', product)
+      this.$emit("add-product", product);
     },
 
     clearInputs() {
@@ -121,7 +121,7 @@ export default {
     },
   },
 
-  computed:{
+  computed: {
     validForm() {
       return this.inputName && this.inputImage && this.inputPrice
         ? true
@@ -131,4 +131,217 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@mixin point {
+  display: inline-block;
+  position: relative;
+  top: -5px;
+  content: " ";
+  width: 4px;
+  height: 4px;
+  background: #ff8484;
+  border-radius: 4px;
+}
+
+.main {
+  &__forms {
+    @media (max-width: 1170px) {
+      flex: 0 1 35%;
+    }
+    @media (max-width: 992px) {
+      flex: 0 1 50%;
+    }
+    @media (max-width: 768px) {
+      flex: 0 0 100%;
+    }
+    flex: 0 0 24%;
+    padding: 24px;
+    background: #fffefb;
+    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04),
+      0px 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+
+    max-height: 435px;
+  }
+
+  &__form {
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 13px;
+    letter-spacing: -0.02em;
+    color: #49485e;
+  }
+}
+
+.form {
+  &__name {
+    &::after {
+      @include point;
+    }
+  }
+
+  &__input-block {
+    margin: 0 0 16px 0;
+    position: relative;
+  }
+
+  &__input {
+    background: #fffefb;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    width: 100%;
+    height: 36px;
+    padding: 0.5rem;
+    &:focus {
+      background-color: #eeeeee;
+      border-bottom: 1px solid #5264ae;
+    }
+  }
+
+  &__completion-message {
+    font-weight: 400;
+    font-size: 8px;
+    line-height: 10px;
+    letter-spacing: -0.02em;
+    color: #ff8484;
+    margin-top: 4px;
+    position: absolute;
+  }
+
+  &__description {
+  }
+
+  &__image {
+    &::after {
+      @include point;
+    }
+  }
+
+  &__price {
+    &::after {
+      @include point;
+    }
+  }
+
+  &__button {
+    background: #eeeeee;
+    border-radius: 10px;
+    width: 100%;
+    height: 36px;
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 15px;
+    letter-spacing: -0.02em;
+    color: #b4b4b4;
+    &:hover {
+      transform: scale(1.02);
+      transition-duration: 200ms;
+    }
+  }
+}
+
+.active {
+  background: #7bae73;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  color: #ffffff;
+}
+
+.text {
+  margin: 0 0 5px 0;
+}
+
+.input-description {
+  height: 108px;
+  resize: none;
+  margin-bottom: 16px;
+}
+
+.product {
+  @media (max-width: 1170px) {
+    flex: 0 1 50%;
+  }
+  @media (max-width: 992px) {
+    flex: 0 1 100%;
+  }
+  @media (max-width: 768px) {
+    flex: 0 1 100%;
+  }
+  flex: 0 1 33.3333%;
+
+  &__container {
+    @media (max-width: 768px) {
+      margin: 0 0 16px 0;
+    }
+    margin: 0 0 16px 16px;
+    background: #fffefb;
+    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04),
+      0px 6px 10px rgba(0, 0, 0, 0.02);
+    border-radius: 4px;
+    position: relative;
+    cursor: pointer;
+    &:hover {
+      transform: scale(1.03);
+      transition-duration: 200ms;
+    }
+  }
+
+  &__image {
+    @media (max-width: 768px) {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    & img {
+      @media (max-width: 992px) {
+        max-width: 100%;
+        max-height: 100%;
+      }
+      @media (max-width: 768px) {
+        max-width: 100%;
+        max-height: 100%;
+      }
+      width: 100%;
+      height: 100%;
+      max-width: 332px;
+      max-height: 200px;
+      object-fit: cover;
+    }
+  }
+
+  &__name {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 25px;
+    color: #3f3f3f;
+    margin: 16px;
+  }
+
+  &__description {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    color: #3f3f3f;
+    margin: 16px;
+  }
+
+  &__price {
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 30px;
+    color: #3f3f3f;
+    margin: 32px 0 0 16px;
+    padding-bottom: 24px;
+  }
+
+  &__delete-button {
+    width: 32px;
+    height: 32px;
+    background-color: #ff8484;
+    position: absolute;
+    right: -8px;
+    top: -8px;
+    border-radius: 10px;
+  }
+}
+</style>
